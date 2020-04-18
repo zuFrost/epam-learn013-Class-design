809f5ab886be9b26d0aa5c1ac34068c2c1f57b6d
Alex Frost <zufrost.a@gmail.com>
2020-04-18 10:22:08 +0300

    add Person and Student classes.

---
+++ src/com/zufrost/learn/Subclassing/Person.java
@@ -0,0 +1,7 @@
+package com.zufrost.learn.Subclassing;
+
+public class Person {
+    private int id;
+    private String name = "";
+    // some metods
+}
---
+++ src/com/zufrost/learn/Subclassing/Student.java
@@ -0,0 +1,5 @@
+package com.zufrost.learn.Subclassing;
+
+public class Student extends Person{
+    private int yearOfStudy;
+}
